a216c2aeefd320093131d1b7437bb4fb24bc77b5
Viktor Ferenczi <viktor@ferenczi.eu>
2022-12-19 00:41:09 +0100

    Refactoring

--- SpaceEngineersScripts/Inventory/CentralInventory.cs
+++ SpaceEngineersScripts/Inventory/CentralInventory.cs
@@ -1,159 +1,12 @@
-/* Central Inventory
-
-Periodically builds a summary on cargo and power status.
-Displays human readable summary on text panels.
-Produces machine readable information to be used by other programs.
-Optionally sorts items into specific containers.
-
-=== How to set up inventory display ===
-
-Build a programmable block.
-Copy-paste all code from the CodeEditor region below into the block.
-Compile and run the code in the block.
-
-Build large text panels with the following words in their name (case insensitive):
-- Resource
-- Ore
-- Ingot
-- Component
-- Ammo
-- Other
-- Status
-- Log
-- Raw
-
-Assign your text panels to the "Inventory Panels" group.
-
-Run the programmable block again after making changes to the group.
-
-Text panel content mode, font and character size will be overridden
-by the script in order to nicely fit the contents. Colors are free
-to configure to match your environment.
-
-You may need two Component panels and two Other panels to fit all items.
-All text panels inside each resource type must have the same size.
-Panels of the same type are concatenated in ascending name order.
-
-The Status panel displays top level summaries.
-
-The Error panel displays warnings and errors.
-
-The Raw panel displays raw inventory information in a YAML like format.
-It can be used by compatible programs to quickly acquire inventory
-information without walking on all the blocks again.
-
-=== How to set up inventory sorting ===
-
-Sorting uses the same programmable block as the display functionality,
-do not add a separate one. You can use only sorting by setting up the
-programmable block like above, but without the displays configured.
-
-Include the following words in the name of the cargo blocks you expect
-the items to be placed into (case insensitive):
-- Ore
-- Ingot
-- Component
-- Weapon
-- Ammo
-- Tool
-- Food
-
-Make sure your have the following words in the name of your gas tanks or
-generator in order to automatically pull bottles for refilling:
-- Hydrogen or H2
-- Oxygen or O2
-- Generator
-
-Sorting is enabled by assigning your target containers (where you expect
-the sorted items to be placed) to the "Sorted Containers" group.
-
-Run the programmable block again after making changes to the group.
-
-For safety reasons sorting functionality will NOT pull
-- ammo out of any turrets or interior guns
-- ore out of refineries
-- ice out of gas generators
-- ingots out of assemblers
-- ingots (Uranium) out of reactors
-- components out of enabled welders
-- zone chips out from safe zone generators
-
-Sorting collects items from the programmable block's own grid.
-This is to prevent pulling out cargo from all docked ships.
-
-=== How to set up restocking of components ===
-
-It is useful to restock the components automatically if the inventory
-drops below a certain level, so welding can continue uninterrupted
-without manually managing the assembler queues.
-
-Restocking of components can be enabled by assigning your designated
-assemblers to the "Restock Assemblers" group. Only the components seen
-in the inventory are restocked. Build one of any new component to 
-trigger restocking.
-
-The minimum stock amount is the same for all components and is defined
-at the top of the source code, search for RESTOCK_MINIMUM. The default 
-value is 10.
-
-If welding interrupts often due to lack of components, then modify
-RESTOCK_OVERHEAD to be higher. The default value is 20% of 
-RESTOCK_MINIMUM (rounded down).
-
-The minimum inventory amounts will be RESTOCK_MINIMUM + RESTOCK_OVERHEAD
-or more if no components are consumed and the assembler queue is fully
-processes. This is the "steady state" minimum inventory.
-
-In order to remove a component from restocking turn OFF the PB, then 
-remove all of that component from the grid completely. When the PB is
-turned ON the next time it will not find that component, therefore no
-restocking will happen. 
-
-=== Remarks ===
-
-Display updates and sorting will be slower if you have more blocks.
-It is normal and the script is designed this way to prevent the PB
-from burning down on multiplayer servers.
-
-You can adjust BATCH_SIZE to change the block scanning speed, higher
-value will result in faster processing at the cost of more computation
-and higher risk of your PB ending up in smoke and fire. If your
-PB burns down, then try to decrease BATCH_SIZE. It may need to be
-tuned for the server you are playing on.
-
-Mod compatibility is entirely untested. Works well with vanialla Space Engineers
-in Experimental mode and scripts enabled as of 2019-11-07. Also works
-on Alehouse PvP Two multiplayer server.
-
-=== Credits ===
-
-Resource name tables were taken from Projector2LCD by Juggernaut93:
-https://steamcommunity.com/sharedfiles/filedetails/?id=1500259551
-
-*/
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Sandbox.ModAPI.Ingame;
-using Skeleton;
 using VRage;
 using VRage.Game;
+using VRage.Game.GUI.TextPanel;
 using VRage.Game.ModAPI.Ingame;
-using ContentType = VRage.Game.GUI.TextPanel.ContentType;
-using IMyAssembler = Sandbox.ModAPI.Ingame.IMyAssembler;
-using IMyBatteryBlock = Sandbox.ModAPI.Ingame.IMyBatteryBlock;
-using IMyCargoContainer = Sandbox.ModAPI.Ingame.IMyCargoContainer;
-using IMyGasGenerator = Sandbox.ModAPI.Ingame.IMyGasGenerator;
-using IMyReactor = Sandbox.ModAPI.Ingame.IMyReactor;
-using IMyRefinery = Sandbox.ModAPI.Ingame.IMyRefinery;
-using IMyShipWelder = Sandbox.ModAPI.Ingame.IMyShipWelder;
-using IMyTerminalBlock = Sandbox.ModAPI.Ingame.IMyTerminalBlock;
-using IMyTextPanel = Sandbox.ModAPI.Ingame.IMyTextPanel;
-using IMyTextSurface = Sandbox.ModAPI.Ingame.IMyTextSurface;
-using IMyUserControllableGun = Sandbox.ModAPI.Ingame.IMyUserControllableGun;
-using MyInventoryItem = VRage.Game.ModAPI.Ingame.MyInventoryItem;
 
 namespace SpaceEngineersScripts.Inventory
 {
@@ -162,7 +15,7 @@ namespace SpaceEngineersScripts.Inventory
         
     }
     
-    public class Program: SpaceEngineersProgram
+    public class Program: MyGridProgram
     {
         #region CodeEditor
 
---
+++ SpaceEngineersScripts/RobotArm/README.md
@@ -0,0 +1,23 @@
+# Robotic Welder Arm Control v1
+
+## Requirements
+- Experimental mode enabled in the game (so the Programmable Block works)
+- [Multigrid Projector client plugin](https://steamcommunity.com/sharedfiles/filedetails/?id=2415983416)
+
+## How to test the script
+- Subscribe to [this world](https://steamcommunity.com/sharedfiles/filedetails/?id=2517203096)
+- Create and enter the world
+- Join the Testers faction
+- Spawn to the medical room
+- Enter the pilot seat at the top
+- Recompile the program in the **Shipyard Projector**
+
+## Remarks
+- Tested only in single player in creative mode so far.
+- Should work in multiplayer with simpler / less number of arms.
+- The video shows an earlier iteration with more arms. The new code works better with less arms.
+- The code may be refactored later to improve its usability in different worlds and for other use cases.
+- Works best with skinny blueprints, primarily small block ones. Solid large block ships are blocking out the arms.
+- Yes, you can build your own robot arms. The base must be in the "Welder Arms" group. Simpler is better.
+
+*Enjoy*
